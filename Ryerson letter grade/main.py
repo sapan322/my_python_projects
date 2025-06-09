95f5fcfdb8915b39dd8babd689ad4767c025887f
Raman Sapunou <43480244+sapan322@users.noreply.github.com>
2025-06-09 09:53:55 +0200

    Add files via upload

---
+++ Ryerson letter grade/main.py
@@ -0,0 +1,16 @@
+def ryerson_letter_grade(pct):
+    grade_dict = {
+        "A+" : 90, "A" : 85, "A-" : 80,
+        "B+" : 77, "B" : 73, "B-" : 70,
+        "C+" : 67, "C" : 63, "C-" : 60,
+        "D+" : 57, "D" : 53, "D-" : 50
+    }
+
+    for grade, points in grade_dict.items():
+        if pct >= points:
+            return grade
+    return "F"
+
+if __name__ == '__main__':
+    print(ryerson_letter_grade(89))
+
